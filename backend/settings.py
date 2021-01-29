aacc88c26ced54a2f6d068cf4e54e71c3f15e42e
human1123 <selliott522@gmail.com>
2021-01-29 15:47:06 -0700

    Finalized settings.py for production build/deploy.

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,6 @@ import django_heroku
 import dj_database_url
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Project-env variable for secret key & db info
@@ -24,19 +23,14 @@ dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# Change before deploy!
 SECRET_KEY = os.environ['SECRET_KEY']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '0.0.0.0',
-    'localhost',
-    '127.0.0.1',
+    # '0.0.0.0',
+    # 'localhost',
+    # '127.0.0.1',
     'django-react-todo-v1.herokuapp.com'
 ]
 
@@ -93,12 +87,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 # On deploy:
 # push local database:PGUSER=postgres PGPASSWORD=password ...>
 #   heroku pg:push postgres://name_of_host/name_of_local_database nameOfHerokuDB
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -113,8 +104,6 @@ db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -131,8 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -146,20 +133,19 @@ USE_TZ = True
 django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')   # Static collection
-STATICFILES_DIRS = []    # Static storage
+STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')     # Static collection
+STATICFILES_DIRS = []                                       # Static storage
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ORIGIN_WHITELIST = [
-'http://localhost:3000',
-'http://127.0.0.1:3000'
-]
+# CORS_ORIGIN_WHITELIST = [
+# 'http://localhost:3000',
+# 'http://127.0.0.1:3000'
+# ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://0.0.0.0:3000',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+    # 'http://0.0.0.0:3000',
+    # 'http://localhost:3000',
+    # 'http://127.0.0.1:3000',
     'https://django-react-todo-v1.herokuapp.com'
 ]
